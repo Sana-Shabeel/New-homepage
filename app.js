363d2b0b59c48f0e06949dc826d1e901bac9241b
Sana_Shabeel <sanastrobarre99@gmail.com>
2022-12-05 16:46:37 +0300

    Responsive navbar

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("app");
+const burgerMenu = document.querySelector(".fa-bars");
+const navLinks = document.querySelector(".nav-link");
+
+burgerMenu.addEventListener("click", (e) => {
+  console.log("clicked menu");
+  navLinks.classList.toggle("hide-links");
+});
