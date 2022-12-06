1a270385408f2b8193c1ba5170e2f15fbb2b0bf9
Sana_Shabeel <sanastrobarre99@gmail.com>
2022-12-06 13:54:09 +0300

    Style sidebar

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const burgerMenu = document.querySelector(".fa-bars");
 const navLinks = document.querySelector(".nav-link");
+const hideSidemenu = document.querySelector(".fa-x");
+const sidemenu = document.querySelector("aside");
+// const sidemenu = document.querySelector(".side-bar-links ");
 
 burgerMenu.addEventListener("click", (e) => {
   console.log("clicked menu");
-  navLinks.classList.toggle("hide-links");
+  sidemenu.classList.toggle("hide");
+});
+
+hideSidemenu.addEventListener("click", (e) => {
+  sidemenu.classList.toggle("hide");
 });
