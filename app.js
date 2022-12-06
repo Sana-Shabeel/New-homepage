b9de1587eacaf97ae0d35d77efca90a273ae1753
Sana_Shabeel <sanastrobarre99@gmail.com>
2022-12-06 13:56:27 +0300

    Remove duplicate code

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const burgerMenu = document.querySelector(".fa-bars");
-const navLinks = document.querySelector(".nav-link");
 const hideSidemenu = document.querySelector(".fa-x");
 const sidemenu = document.querySelector("aside");
-// const sidemenu = document.querySelector(".side-bar-links ");
 
 burgerMenu.addEventListener("click", (e) => {
   console.log("clicked menu");
